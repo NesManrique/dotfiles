c3f9a0f0a2f0fdf4915cf75f587cf8595dbd5387
Nestor Manrique <nes.manrique@gmail.com>
2024-01-04 11:26:26 +0100

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,13 @@ sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 git clone git@github.com:NesManrique/dotfiles.git /home/nesmanrique/dotfiles
 
 # Enable flakes
-cp dotfiles/nix/nix.conf /etc/nix/nix.conf
+sudo cp dotfiles/nix/nix.conf /etc/nix/
 sudo systemctl restart nix-daemon
 
 # Copy home-manager flake and config
+mkdir -p ~/.config/
 cp -r dotfiles/nix/home-manager ~/.config/home-manager
 
 # Activate home-manager
-nix run home-manager/23.11 -- init --switch
+nix run home-manager/release-23.11 -- init --switch
 
